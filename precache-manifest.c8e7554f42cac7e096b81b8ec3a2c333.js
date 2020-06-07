1ddad0193689b21dd7afe3444e24f5cd57d94061
KateAstrid <63353519+KateAstrid@users.noreply.github.com>
2020-06-07 13:23:35 +0300

    initial commit

---
+++ precache-manifest.c8e7554f42cac7e096b81b8ec3a2c333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b13fb534d5e01f95d3b989648411ef0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4a3fa18d51b92eb437a",
+    "url": "./static/css/main.9eb9f287.chunk.css"
+  },
+  {
+    "revision": "b6418c9b5199842b930a",
+    "url": "./static/js/2.79232574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.79232574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a3fa18d51b92eb437a",
+    "url": "./static/js/main.0c7779aa.chunk.js"
+  },
+  {
+    "revision": "92f9c0c00e5694af4c53",
+    "url": "./static/js/runtime-main.d6a4d2a5.js"
+  }
+]);
